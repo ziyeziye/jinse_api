f7e567f0452697db24f1ee83669b0f1764136940
ziyeziye <547562496@qq.com>
2020-03-24 17:53:26 +0800

    up

--- routes/jinse.php
+++ routes/jinse.php
@@ -23,42 +23,43 @@ Route::group(['prefix' => '/api'], function () {
         Route::get('/user', 'ApiAuthController@user');
         Route::get('/logout', 'ApiAuthController@logout');
 
-
-        Route::get('/banners', 'BannerController@table');
-        Route::get('/banners/{id}', 'BannerController@info');
-
         Route::get('/users', 'UserController@table');
         Route::get('/users/{id}', 'UserController@info');
         Route::put('/users/{id}', 'UserController@update');
 
-        Route::get('/tags', 'TagController@table');
-        Route::get('/tags/{id}', 'TagController@info');
 
-        Route::get('/hot_words', 'HotWordController@table');
-        Route::get('/hot_words/{id}', 'HotWordController@info');
+    });
 
-        Route::get('/articles', 'ArticleController@table');
-        Route::get('/articles/{id}', 'ArticleController@info');
+    Route::get('/banners', 'BannerController@table');
+    Route::get('/banners/{id}', 'BannerController@info');
 
-        Route::get('/categories', 'CategoryController@table');
-        Route::get('/categories/group', 'CategoryController@group');
-        Route::get('/categories/{id}', 'CategoryController@info');
+    Route::get('/tags', 'TagController@table');
+    Route::get('/tags/{id}', 'TagController@info');
 
-        Route::get('/subjects', 'SubjectController@table');
-        Route::get('/subjects/{id}/articles', 'SubjectController@articles');
-        Route::get('/subjects/{id}', 'SubjectController@info');
+    Route::get('/hot_words', 'HotWordController@table');
+    Route::get('/hot_words/{id}', 'HotWordController@info');
 
-        Route::get('/pages', 'PageController@table');
-        Route::get('/pages/{id}', 'PageController@info');
+    Route::get('/articles', 'ArticleController@table');
+    Route::get('/articles/{id}', 'ArticleController@info');
 
-        Route::get('/notices', 'NoticeController@table');
-        Route::get('/notices/{id}', 'NoticeController@info');
+    Route::get('/categories', 'CategoryController@table');
+    Route::get('/categories/group', 'CategoryController@group');
+    Route::get('/categories/{id}', 'CategoryController@info');
+
+    Route::get('/subjects', 'SubjectController@table');
+    Route::get('/subjects/{id}/articles', 'SubjectController@articles');
+    Route::get('/subjects/{id}', 'SubjectController@info');
+
+    Route::get('/pages', 'PageController@table');
+    Route::get('/pages/{id}', 'PageController@info');
+
+    Route::get('/notices', 'NoticeController@table');
+    Route::get('/notices/{id}', 'NoticeController@info');
 
 //        Route::get('/comments', 'CommentController@table');
 //        Route::get('/comments/{id}', 'CommentController@info');
-        Route::post('/comments', 'CommentController@save');
+    Route::post('/comments', 'CommentController@save');
 //        Route::put('/comments/{id}', 'CommentController@update');
 //        Route::delete('/comments', 'CommentController@delete');
 
-    });
 });
